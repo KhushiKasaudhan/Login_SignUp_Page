72be5dfe4dc660169861d2b9d35f65878121f48d
KhushiKasaudhan <khushikasaudhan8041@gmail.com>
2023-04-06 10:35:57 +0530

    My Commit

---
+++ loginsignup/lib/main.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:loginsignup/login.dart';
+import 'package:loginsignup/register.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: 'login',
+    routes: {
+      'login': (context) => MyLogin(),
+      'register': (context) => MyRegister()
+    },
+  ));
+}
+
+
+
+  
